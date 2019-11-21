e04ea622498b709ac326d220bb2c1e7f82939335
Joey Parrish <joeyparrish@users.noreply.github.com>
2019-11-21 14:05:31 -0800

    Add demo page (#5)
    * Add demo and manual testing page
    
    This helped expose several issues which will be fixed in follow-up
    commits.  This also provides a platform for testing future
    MediaCapabilities support in the polyfill.
    
    Issue #1
    Issue #2
    Issue #3
    Issue #4

---
+++ demo/load-tests/test-load.js
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function checkDefinition(polyfill) {
+  console.debug('Polyfill:', polyfill, polyfill && polyfill.install);
+  const polyfillIsCorrectlyDefined = !!polyfill && !!polyfill.install;
+  return polyfillIsCorrectlyDefined;
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const title = document.title.split('-')[1];
+
+  if (window.parent != window) {
+    const popOutButton = document.createElement('button');
+    popOutButton.textContent = 'pop out';
+    popOutButton.style.marginRight = '1em';
+    popOutButton.addEventListener('click', () => {
+      window.parent.location = location.href;
+    });
+    document.body.appendChild(popOutButton);
+  }
+
+  const titleDiv = document.createElement('span');
+  titleDiv.textContent = `Testing ${title}`;
+  document.body.appendChild(titleDiv);
+
+  const status = document.createElement('span');
+  titleDiv.appendChild(status);
+
+  status.textContent = '...';
+
+  try {
+    const okay = await testLoader() ? 'OK' : 'FAILED!';
+    status.textContent = `: ${okay}`;
+  } catch (error) {
+    status.textContent = `: Error "${error.message}"`;
+  }
+});
---
+++ demo/query.js
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+document.addEventListener('DOMContentLoaded', () => {
+  const formatObjectToString = (obj) => JSON.stringify(obj, null, 2);
+
+  window.emeRun.addEventListener('click', async () => {
+    // Pull contents of query form.
+    const keySystem = window.keySystem.input.value;
+    // If encryptionScheme is blank, default to null.
+    const encryptionScheme = window.encryptionScheme.input.value || null;
+    const audio = window.audio.input.value;
+    const video = window.video.input.value;
+
+    const config = {};
+
+    if (audio) {
+      config.audioCapabilities = [{
+        contentType: audio,
+        encryptionScheme,
+      }];
+    }
+
+    if (video) {
+      config.videoCapabilities = [{
+        contentType: video,
+        encryptionScheme,
+      }];
+    }
+
+    try {
+      const mksa =
+          await navigator.requestMediaKeySystemAccess(keySystem, [config]);
+      window.results.textContent =
+          formatObjectToString(mksa.getConfiguration());
+    } catch (error) {
+      window.results.textContent = formatObjectToString({error: error.message});
+    }
+  });  // emeRunButton click listener
+});  // DOMContentLoaded listener
