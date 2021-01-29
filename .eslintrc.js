e7ce01590dc54ed3257c8eee97a541837e5e2a25
Joey Parrish <joeyparrish@google.com>
2021-01-28 16:29:18 -0800

    chore: Remove the term "blacklist"
    The term "blacklist" violates new Google guidelines about respectful
    language.  This removes all instances of this term in our repo and
    replaces them with "denylist".
    
    Bug: 178201531

--- .eslintrc.js
+++ .eslintrc.js
@@ -130,7 +130,7 @@ module.exports = {
     'array-bracket-newline': ['error', 'consistent'],
     'block-spacing': ['error', 'always'],
     'brace-style': ['error', '1tbs', {'allowSingleLine': true}],
-    'id-blacklist': ['error', 'async'],
+    'id-denylist': ['error', 'async'],
     'lines-between-class-members': 'error',
     'max-statements-per-line': ['error', {'max': 1}],
     'new-parens': 'error',
